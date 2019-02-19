ab8666b2642e6cfee80ba1634bdffacab6cb3087
morromio <morromio@gmail.com>
2019-02-19 14:46:25 +0100

    Changes to master (Roles)

---
+++ AuthSys/AuthSys/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using AuthSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AuthSys.ViewModels;
+
+namespace AuthSys.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddAdmin()
+        {
+            return View("AddAdmin");
+        }
+
+        [HttpPost]
+        public ActionResult AddAdmin(AdminViewModel model)
+        {
+            return View("AddAdmin");
+        }
+    }
+}
---
+++ AuthSys/AuthSys/Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthSys.Models
+{
+    public class Admin
+    {
+        public string MemberID { get; set; }
+
+        public string MemberEmail { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
